a0c475d5a2aeda982e02cdba402ee0bc6b341e37
crow-dz <walid.cena@gmail.com>
2024-05-09 07:03:03 +0100

    Handdling multi requests

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -10,6 +10,17 @@ $id = $_GET['id'];
 
 $currentUser = 8;
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Auth Layer
+    $note = $db->query('SELECT * FROM notes where id= :id ', ['id' => $id])->findOrFail();
+    authorize($note['user_id'] !== $currentUser);
+    // Delete Action
+    $db->query('DELETE from notes where id= :id', ['id' => $_POST['id']]);
+    // Redirect
+    header('Location:/notes');
+    exit();
+}
+
 $note = $db->query('SELECT * FROM notes where id= :id ', ['id' => $id])->findOrFail();
 
 authorize($note['user_id'] !== $currentUser);
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -5,5 +5,9 @@
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <p><?= htmlentities($note['body']) ?></p>
     </div>
+    <form class="m-2" method="POST">
+        <input name="id" type="text" hidden value="<?= $note['id'] ?>">
+        <button class="text-red-400 " type="submit">Delete</button>
+    </form>
 </main>
 <?php require(base_dir('views/partails/footer.php')) ?>
